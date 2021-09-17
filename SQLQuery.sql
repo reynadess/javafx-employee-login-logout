9d34f0f0337dbeb09a4bb6054871d8fde1676eec
Chaitanya Mphs <chaitanya.raparti@medplusindia.com>
2021-09-17 12:27:49 +0530

    Updated SQLQuery

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE employees;
 
-CREATE TABLE employees(
+
+CREATE TABLE employees.employees(
 employee_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 employee_name VARCHAR(100) NOT NULL,
 employee_password VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL
 );
 
+INSERT INTO employees.employees(employee_name, employee_password, email) VALUES ("admin", 'admin', 'admin@medplus.in');
